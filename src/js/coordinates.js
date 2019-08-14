505ee307950f955ca77ed4a346af1d2f31b9b2bb
Robin C. Ladiges <rcl.git@blackpinguin.de>
2019-08-14 20:30:36 +0200

    popup: not using the default `vertical-align: middle;` of tables

--- src/js/coordinates.js
+++ src/js/coordinates.js
@@ -2,7 +2,7 @@ L.Control.Coordinates = L.Control.extend({
   onAdd(map) {
     const div = L.DomUtil.create('div')
     div.classList.add('pine-mouse-coordinates', 'leaflet-bar')
-    div.innerHTML = '(0.0, 0.0)'
+    div.innerHTML = '(0, 0)'
     this.mousehandler = (ev) => div.innerHTML = '(' + Math.round(ev.latlng.lng - 1024) + ', ' + Math.round(ev.latlng.lat + 1024) + ')'
     map.addEventListener('mousemove', this.mousehandler)
     return div
