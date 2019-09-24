42ff1766aa4517250efc0b020bd7d4acc50caa3f
Robin C. Ladiges <rcl.git@blackpinguin.de>
2019-09-24 21:07:45 +0200

    wikify: more abstract and links to areas

--- wikify.sh
+++ wikify.sh
@@ -15,7 +15,7 @@ mkdir -p "$WIKI"
 
 cd "$PRETTY"
 
-like_they_are='alpafant|beagalite|bleeker|bleeker_antenna|carrant|chest|dullrock|fatplant|idea|lunarodos|mudbeet|nuctus|obergine|puffle|puffle_feather|sandstone|solfodil|spystal|tingflower|toothstone|waddletooth'
+like_they_are='alpafant|beagalite|bleeker|carrant|chest|dullrock|fatplant|idea|lunarodos|mudbeet|nuctus|obergine|puffle|sandstone|solfodil|spystal|tingflower|toothstone|waddletooth'
 
 while read file ; do
   name="${file:2:-4}"
@@ -29,32 +29,23 @@ while read file ; do
       -e 's/;/,/g'                                                   \
       -e 's/(\|\|[^\|]+)$/||  ||  || game assets \1/'                \
       -e "s/\| ($like_they_are) \|/| [[\1]] |/gi"                    \
-      -e 's/\| Alpafant_Leather \|/| [[Alpafant Leather]] |/g'       \
-      -e 's/\| Amphiscis_Orb \|/| [[Amphiscis Orb]] |/g'             \
-      -e 's/\| AnuraShroom \|/| [[Anura Shroom]] |/g'                \
-      -e 's/\| AvianPepper \|/| [[Avian Pepper]] |/g'                \
-      -e 's/\| Bleeker_Antenna \|/| [[Bleeker Antenna]] |/g'         \
       -e 's/\| Bone \|/| [[Crassbone]] |/g'                          \
       -e 's/\| DouglasCone \|/| [[Grandcone]] |/g'                   \
-      -e 's/\| DryClay \|/| [[Dry Clay]] |/g'                        \
       -e 's/\| DuneRice \|/| [[Dunerice]] |/g'                       \
-      -e 's/\| EdenFruit \|/| [[Eden Fruit]] |/g'                    \
       -e 's/\| food \|/| [[Category:Food|Food]] |/g'                 \
       -e 's/\| Iron \|/| [[Lean-Iron]] |/g'                          \
-      -e 's/\| KeyGraphite \|/| [[Key Graphite]] |/g'                \
       -e 's/\| material \|/| [[Category:Material|Material]] |/g'     \
-      -e 's/\| MorrowHay \|/| [[Morrow Hay]] |/g'                    \
       -e 's/\| Pearl \|/| [[Slick Pearl]] |/g'                       \
-      -e 's/\| Puffle_Feather \|/| [[Puffle Feather]] |/g'           \
-      -e 's/\| PuffleEgg \|/| [[Puffle Egg]] |/g'                    \
       -e 's/\| Redberry \|/| [[Roseberry]] |/g'                      \
-      -e 's/\| RidgeFennel \|/| [[Ridge Fennel]] |/g'                \
       -e 's/\| SmallYam \|/| [[Meageryam]] |/g'                      \
-      -e 's/\| TelkinChives \|/| [[Telkin Chives]] |/g'              \
-      -e 's/\| WaddletoothBlubber \|/| [[Waddletooth Blubber]] |/g'  \
       -e 's/\| WedgewoodMoss \|/| [[Gravel Moss]] |/g'               \
       -e 's/\| Wheat \|/| [[Commonwheat]] |/g'                       \
       -e 's/\| Wood \|/| [[Marrwood]] |/g'                           \
+      -e 's/\| NewHome \|/| RemoteSwamps |/g'                        \
+      -e 's/\| ([A-Z][a-z]+)(([A-Z][a-z]+)+) \|/| [[\1\2]] |/g'      \
+      -e 's/\| ([A-Z][a-z]+)((_[A-Z][a-z]+)+) \|/| [[\1\2]] |/g'     \
+      -e 's/([a-z])([A-Z])/\1 \2/g'                                  \
+      -e 's/([a-z])_([A-Z])/\1 \2/g'                                 \
       -e 's/^/|-\n| /'                                               \
       "$file"                                                        \
     | tail -n+3 >"$out"                                              \
