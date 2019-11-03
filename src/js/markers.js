Istador/pine-interactive-map

commit dd79157902e05cf96574026a9d823bcb99d9ad56

villages, update layers, tooltips and beagalite icon
- new layer: npc.village
- unique.{chest,idea} => idea.{chest,pickup,quest}
- change default layers (-npc.quest, +entrance.cave, +npc.village)
- work with old layer names as input by replacing them
- custom tooltip title handling for quests and ideas
- change beagalite color and size to look more like in-game

src/js/markers.js
@@ -19,6 +19,11 @@ const tints = {
     n: 5,
     i: 0,
   },
+  nest: {
+    map: {},
+    n: 4,
+    i: 6,
+  },
   spawn: {
     map: {},
     n: 4,
@@ -133,7 +138,15 @@ const popup = (row, marker) => {
 
 const tooltip = (row) =>
   (row.amount && ! isNaN(Number(row.amount)) ? row.amount + 'x ' : '')
-    + item2name(row.type)(row.item)
+    + (
+      row.item === 'quest' && [ 'idea', 'item' ].includes(row.type)
+      ? item2name(row.type)(row.item) + ' ' + type2name(row.type)
+      : (
+        row.type === 'idea' || row.type === 'npc' && row.item === 'quest'
+        ? type2name(row.type)
+        : item2name(row.type)(row.item)
+      )
+    )
     + (row.title ? ': ' + row.title : '')
     + '<br/>(' + row.x + ', ' + row.z + ')'
     + (row.area ? ' in ' + row.area : '')

src/css/icons.scss
@@ -80,6 +80,11 @@ div.leaflet-marker-pane > div.pine-marker {
     @include tint(16);
   }
 
+  // idea
+  &.pine-marker-idea {
+    @include scaled($pine-icon-chest);
+  }
+
   // item
   &.pine-marker-item {
     @include scaled($pine-icon-equip, 0.6);
@@ -93,7 +98,8 @@ div.leaflet-marker-pane > div.pine-marker {
 
     // beagalite
     &.pine-marker-material-beagalite {
-      @include scaled($pine-icon-beagalite);
+      @include scaled($pine-icon-beagalite, 0.5, 0.7);
+      filter: hue-rotate(90deg) brightness(2);
     }
     // dullrock
     &.pine-marker-material-dullrock {
@@ -121,7 +127,7 @@ div.leaflet-marker-pane > div.pine-marker {
     // sandstone
     &.pine-marker-material-sandstone {
       @include scaled($pine-icon-beagalite);
-      filter: hue-rotate(90deg) brightness(2);
+      filter: hue-rotate(90deg) brightness(2.5) saturate(0.75);
     }
     // spystal
     &.pine-marker-material-spystal {
@@ -142,15 +148,23 @@ div.leaflet-marker-pane > div.pine-marker {
     @include tint(5);
   }
 
+  // npc
+  &.pine-marker-npc-village
+  {
+    @include scaled($pine-icon-village);
+  }
+
   // spawn
-  &.pine-marker-spawn {
+  &.pine-marker-spawn
+  , &.pine-marker-nest {
     // default
     @include scaled($pine-icon-nest);
     @include tint(4);
   }
 
   // quest
-  &.pine-marker-item-quest
+  &.pine-marker-idea-quest
+  , &.pine-marker-item-quest
   , &.pine-marker-npc-quest
   , &.pine-marker-entrance-quest
   , &.pine-marker-entrance-vault
@@ -164,19 +178,9 @@ div.leaflet-marker-pane > div.pine-marker {
     &.pine-marker-unique-amphiscusorb {
       @include scaled($pine-icon-emblem);
     }
-    // emblem
-    &.pine-marker-unique-emblem {
-      @include scaled($pine-icon-emblem);
-    }
     // keygraphite
     &.pine-marker-unique-keygraphite {
       @include scaled($pine-icon-keygraphite, 10/32);
-    }
-    // chest, idea
-    &.pine-marker-unique-chest
-    , &.pine-marker-unique-idea
-    {
-      @include scaled($pine-icon-chest);
     }
     // journal / book
     &.pine-marker-unique-journal {

src/js/datasource.js
@@ -1,6 +1,6 @@
 const escapeHtml = (str) => str.replace(/[&<>'"]/g, x => '&#' + x.charCodeAt(0) + ';')
 
-const { capitalize } = require('./names')
+const { capitalize, mapOldBoth } = require('./names')
 const { version } = require('./selection')
 
 const text2number = (text) => {
@@ -76,6 +76,9 @@ const transform = {
             obj[key] = Math.round(obj[key] * 100) / 100
           }
         }
+        const [ type, item ] = mapOldBoth(obj.type, obj.item)
+        obj.type = type
+        obj.item = item
         out.push(obj)
       }
       return out

src/js/i18n.js
@@ -2,15 +2,15 @@ const { storage } = require('./util')
 
 
 const data = {
-  en: require('./i18n/en.js'),
-  de: require('./i18n/de.js'),
-  fr: require('./i18n/fr.js'),
-  it: require('./i18n/it.js'),
-  es: require('./i18n/es.js'),
-  //zh: require('./i18n/zh.js'),
-  //ru: require('./i18n/ru.js'),
-  pg: require('./i18n/pg.js'),
-  //du: require('./i18n/du.js'),
+  en: require('./i18n/en'),
+  de: require('./i18n/de'),
+  fr: require('./i18n/fr'),
+  it: require('./i18n/it'),
+  es: require('./i18n/es'),
+  //zh: require('./i18n/zh'),
+  //ru: require('./i18n/ru'),
+  pg: require('./i18n/pg'),
+  //du: require('./i18n/du'),
 }
 
 

src/js/i18n/de.js
@@ -61,6 +61,12 @@ module.exports = {
       tingflower      : 'Tingblume',
       waddletoothloin : 'Watschelzahn-Lende',
     },
+    idea: {
+      '.'    : 'Idee',
+      chest  : 'Truhe',
+      pickup : 'Aufhebbar',
+      quest  : 'Quest',
+    },
     item: {
       '.'   : 'Gegenstand',
       equip : 'Ausrüstung',
@@ -95,8 +101,8 @@ module.exports = {
       door  : 'Tür',
       lever : 'Hebel',
     },
-    spawn: {
-      '.'         : 'Kreaturen',
+    nest: {
+      '.'         : 'Nest',
       alpafant    : 'Alpafant',
       bleeker     : 'Schnapper',
       puffle      : 'Fluffi',
@@ -106,13 +112,18 @@ module.exports = {
       '.'      : 'NPC',
       chief    : 'Häuptling',
       merchant : 'Händler',
+      village  : 'Dorf',
+    },
+    spawn: {
+      '.'         : 'Kreaturen',
+      alpafant    : 'Alpafant',
+      bleeker     : 'Schnapper',
+      puffle      : 'Fluffi',
+      waddletooth : 'Watschelzahn',
     },
     unique: {
       '.'          : 'Einzigartig',
       amphiscusorb : 'Amphiscus-Kugel',
-      chest        : 'Truhe',
-      emblem       : 'Amphiscus-Emblem',
-      idea         : 'Idee',
       journal      : 'Journal / Buch',
       keygraphite  : 'Schlüsselgraphit',
     },

src/js/i18n/en.js
@@ -68,6 +68,12 @@ module.exports = {
       tingflower      : 'Tingflower',
       waddletoothloin : 'Waddletooth Loin',
     },
+    idea: {
+      '.'    : 'Idea',
+      chest  : 'Chest',
+      pickup : 'Pick up',
+      quest  : 'Quest',
+    },
     item: {
       '.'        : 'Item',
       equip      : 'Equipment',
@@ -107,8 +113,8 @@ module.exports = {
       lever          : 'Lever',
       pinsocket      : 'Pin Socket',
     },
-    spawn: {
-      '.'         : 'Spawn',
+    nest: {
+      '.'         : 'Nest',
       alpafant    : 'Alpafant',
       bleeker     : 'Bleeker',
       puffle      : 'Puffle',
@@ -119,13 +125,18 @@ module.exports = {
       chief    : 'Chief',
       merchant : 'Merchant',
       quest    : 'Quest',
+      village  : 'Village',
+    },
+    spawn: {
+      '.'         : 'Spawn',
+      alpafant    : 'Alpafant',
+      bleeker     : 'Bleeker',
+      puffle      : 'Puffle',
+      waddletooth : 'Waddletooth',
     },
     unique: {
       '.'          : 'Unique',
       amphiscusorb : 'Amphiscus Orb',
-      chest        : 'Chest',
-      emblem       : 'Amphiscis Emblem',
-      idea         : 'Idea',
       journal      : 'Journal / Book',
       keygraphite  : 'Key Graphite',
     },

src/js/i18n/es.js
@@ -23,6 +23,12 @@ module.exports = {
       tingflower      : 'Hierba de los dientes',
       waddletoothloin : 'Lomo de Contodiente',
     },
+    idea: {
+      '.'   : 'Idea',
+      //chest  : '',
+      //pickup : '',
+      //quest  : '',
+    },
     material: {
       '.'                : 'Materiales',
       alpafantleather    : 'Piel de Alpafante',
@@ -48,6 +54,13 @@ module.exports = {
       toothstone         : 'Piedra dental',
       waddletoothblubber : 'Grasa de Contodiente',
     },
+    nest: {
+      //'.'         : '',
+      alpafant    : 'Alpafante',
+      bleeker     : 'Bleeker',
+      puffle      : 'Nubesco',
+      waddletooth : 'Contodiente',
+    },
     spawn: {
       '.'         : 'Criatura',
       alpafant    : 'Alpafante',
@@ -57,8 +70,6 @@ module.exports = {
     },
     unique: {
       amphiscusorb : 'Orbes de Anfibu',
-      emblem       : 'Orbes de Anfibu',
-      idea         : 'Idea',
       keygraphite  : 'Grafito de Llave',
     },
   },

src/js/i18n/fr.js
@@ -54,6 +54,12 @@ module.exports = {
       waddletoothloin : 'Longe de Waddletooth',
       commonwheat     : 'Blé Commun',
     },
+    idea: {
+      '.'    : 'Idée',
+      chest  : 'Coffres',
+      //pickup : '',
+      quest  : 'Quêtes',
+    },
     item: {
       '.'        : 'Objet',
       equip      : 'Équipement',
@@ -93,8 +99,8 @@ module.exports = {
       lever          : 'Levier',
       pinsocket      : 'Prise de broche',
     },
-    spawn: {
-      '.'         : 'Créatures',
+    nest: {
+      //'.'         : '',
       alpafant    : 'Alpafant',
       bleeker     : 'Bleeker',
       puffle      : 'Puffle',
@@ -105,13 +111,18 @@ module.exports = {
       chief    : 'Chef',
       merchant : 'Marchant',
       quest    : 'Quêtes',
+      //village  : '',
+    },
+    spawn: {
+      '.'         : 'Créatures',
+      alpafant    : 'Alpafant',
+      bleeker     : 'Bleeker',
+      puffle      : 'Puffle',
+      waddletooth : 'Waddletooth',
     },
     unique: {
       '.'          : 'Unique',
       amphiscusorb : 'Orbes',
-      chest        : 'Coffres',
-      emblem       : 'Emblème d\'amphiscis',
-      idea         : 'Idée',
       journal      : 'Journal / Livre',
       keygraphite  : 'Graphite de Clé',
     },

commit eebcd47dfce9fb6c370419902f7e0b3c089dbd04

bugfix: allow marking ideas as completed again
this bug was introduced with commit
dd79157902e05cf96574026a9d823bcb99d9ad56

src/js/markers.js
@@ -71,7 +71,7 @@ const showProp = (prop) => ! [ 'seen', 'hasUniqueID', 'screenshot' ].includes(pr
 const showCompleteButton = (row) => (
      storage().can()
   && row.hasUniqueID
-  && [ 'unique', 'item', 'npc', 'entrance' ].includes(row.type)
+  && [ 'unique', 'idea', 'item', 'npc', 'entrance' ].includes(row.type)
 )
 
 const updateToggle = (toggle, row) => {