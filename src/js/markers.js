6700377dd72c37f669077379964f2720a6b2c92d
Robin C. Ladiges <rcl.git@blackpinguin.de>
2020-08-22 22:40:24 +0200

    fix: wiki images with no-referrer

--- src/js/markers.js
+++ src/js/markers.js
@@ -93,6 +93,7 @@ const popup = (row, marker) => {
     a.setAttribute('href', `${__WIKI__}File:${filename}`)
     a.setAttribute('target', '_blank')
     const img = L.DomUtil.create('img', '', a)
+    img.setAttribute('referrerpolicy', 'no-referrer')
     img.setAttribute('src', `${__WIKI__}Special:Redirect/file/${filename}?width=240&height=240`)
     // update the popup, to adjust it's size to the image (but only once!)
     if (! row.screenshot_html) {
