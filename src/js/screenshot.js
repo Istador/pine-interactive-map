e03eeabf596595b6a3ed960de6929718440abf66
Robin C. Ladiges <rcl.git@blackpinguin.de>
2021-03-08 01:29:51 +0100

    ie11: iframe to the rescue (*)
    In IE11 the only way to disable the referrer seems to be `<meta
    name="referrer" content="never">`.
    
    But I only want this for images and not globally for all resources. So I
    wrap it inside of an `<iframe>` to separate it from the other content.
    
    Change in behaviour:
    - Before this change it was increasing the size of the popup after
    fetching/loading the image, and then reloading the popup
    (`marker._popup.update()`).
    - Now it reserves the maximum possible space before and then decreases
    the size after fetching, without doing a popup reload. Because with an
    iframe with its own scope, the reload would result in the image being
    fetched two times everytime a popup opens.
    
    Though a general downside of the iframes compared to before, is that it
    fetches the image multiple times when the same popup is opened again.
    But that's what the browser cache is for.
    
    This commit fixes issue #34.

--- src/js/screenshot.js
+++ src/js/screenshot.js
@@ -1,20 +1,35 @@
 const screenshot = (filename, container, marker, row) => {
-  const div = L.DomUtil.create('div', 'pine-screenshot', container)
+  const div  = L.DomUtil.create('div', 'pine-screenshot', container)
+  const wrap = L.DomUtil.create('div', '', div)
 
-  const a = L.DomUtil.create('a', '', div)
-  a.setAttribute('href', `${__WIKI__}File:${filename}`)
-  a.setAttribute('target', '_blank')
-
-  const img = L.DomUtil.create('img', '', a)
-  img.setAttribute('referrerpolicy', 'no-referrer')
-  img.setAttribute('src', `${__WIKI__}Special:Redirect/file/${filename}?width=240&height=240`)
-
-  // update the popup, to adjust it's size to the image (but only once!)
-  if (! row.screenshot_html) {
-    img.onload = () => {
-      row.screenshot_html = 'loaded'
-      if (marker._popup) { marker._popup.update() }
+  // img.referrerpolicy doesn't work in IE11, so put it into an iframe with meta.referrer = never
+  const iframe = L.DomUtil.create('iframe', '', wrap)
+  // contentDocument is only available after the iframe has been added to the DOM, which will happen later, so we have to call the code later with onload
+  iframe.onload = () => {
+    iframe.onload = undefined
+    iframe.contentDocument.write(
+      '<html><head>'
+      + '<meta name="referrer" content="never">'
+      + '<style>*{overflow:hidden;margin:0;}</style>'
+      + '</head><body>'
+      + `<a href="${__WIKI__}File:${filename}" target="_blank">`
+      + `<img src="${__WIKI__}Special:Redirect/file/${filename}?width=240&height=240" referrerpolicy="no-referrer">`
+      + '</a></body></html>'
+    )
+    // stop browser thinking it's infinite loading
+    iframe.contentDocument.close()
+    // set the size based on the image dimensions
+    const img = iframe.contentDocument.querySelector('img')
+    const set = () => {
+      img.onload = undefined
+      iframe.style.width  = img.width  + 'px'
+      iframe.style.height = img.height + 'px'
+      wrap.style.width    = img.width  + 'px'
+      wrap.style.height   = img.height + 'px'
     }
+    // if the image is already loaded, we can set the size immediately
+    if (img.complete) { set() }
+    else { img.onload = set }
   }
 }
 
