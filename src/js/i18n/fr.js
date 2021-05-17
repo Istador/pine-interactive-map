d11064f8f1e6f2e75d5e18e6a9d66019528f27c7
アレチ <17111758+Angelisium@users.noreply.github.com>
2021-05-17 14:51:05 +0200

    Update fr.js
    fix text indent

--- src/js/i18n/fr.js
+++ src/js/i18n/fr.js
@@ -3,8 +3,8 @@ module.exports = {
 		title					: 'Map interactif de Pine',
 		zoom_in					: 'Zoomer',
 		zoom_out				: 'Dézoomer',
-		fullscreen_on			: 'Plein écran',
-		fullscreen_off			: 'Quitter le mode plein écran',
+		fullscreen_on				: 'Plein écran',
+		fullscreen_off				: 'Quitter le mode plein écran',
 		datasource				: 'Contribuer',
 		mark_seen				: 'Marquer comme étant terminé.',
 		unmark_seen				: 'Retirer le marquage.',
@@ -18,7 +18,7 @@ module.exports = {
 			},
 			unconfirmed: {
 				show			: 'Les éléments non-vérifiés sont affichés normalement.',
-				red				: 'Les éléments non-vérifiés sont colorés en rouge.',
+				red			: 'Les éléments non-vérifiés sont colorés en rouge.',
 				hide			: 'Les éléments non-vérifiés sont cachés.',
 				only			: 'Seuls les éléments non-vérifiés sont affichés.'
 			}
@@ -29,9 +29,9 @@ module.exports = {
 		item					: 'Nom',
 		item_id					: 'ID de l\u0027élément', // ID de l'élément
 		amount					: 'Quantité',
-		x						: 'X',
-		y						: 'Y',
-		z						: 'Z',
+		x					: 'X',
+		y					: 'Y',
+		z					: 'Z',
 		area					: 'Zone',
 		title					: 'Nom',
 		description				: 'Description',
@@ -40,41 +40,41 @@ module.exports = {
 	},
 	names: {
 		area: {
-			'.'					: 'Zones',
-			1					: 'Maison', //Falaises instables
-			2					: 'Bois du Coin',
-			3					: 'Champs de Pollen',
-			4					: 'Falaises Côtières',
-			5					: 'Champs Clairsemés',
-			6					: 'Montagnes Concaves',
-			7					: 'Grande Vallée',
-			8					: 'Crête Etroite',
-			9					: 'Côtes Boréales',
-			10					: 'Rivages Humides',
-			11					: 'Sources Poussiéreuses',
-			12					: 'Canyon Aride',
-			13					: 'Dunes de Tunnel',
-			14					: 'Baie Sèche',
-			15					: 'Bords de Crête',
-			16					: 'Bois Innondés',
-			17					: 'Plateaux du Nord',
-			18					: 'Passage de Telkin',
-			19					: 'Mont Telkin',
-			20					: 'Grande Crête',
-			21					: 'Bois Eloignés'
+			'.'				: 'Zones',
+			1				: 'Maison', //Falaises instables
+			2				: 'Bois du Coin',
+			3				: 'Champs de Pollen',
+			4				: 'Falaises Côtières',
+			5				: 'Champs Clairsemés',
+			6				: 'Montagnes Concaves',
+			7				: 'Grande Vallée',
+			8				: 'Crête Etroite',
+			9				: 'Côtes Boréales',
+			10				: 'Rivages Humides',
+			11				: 'Sources Poussiéreuses',
+			12				: 'Canyon Aride',
+			13				: 'Dunes de Tunnel',
+			14				: 'Baie Sèche',
+			15				: 'Bords de Crête',
+			16				: 'Bois Innondés',
+			17				: 'Plateaux du Nord',
+			18				: 'Passage de Telkin',
+			19				: 'Mont Telkin',
+			20				: 'Grande Crête',
+			21				: 'Bois Eloignés'
 		},
 		entrance: {
-			'.'					: 'Entrées', // ?
+			'.'				: 'Entrées', // ?
 			cave				: 'Grottes',
 			mohlenhill			: 'Coline de Mohlen',
 			vault				: 'Voûtes'
 		},
 		food: {
-			'.'					: 'Nourritures',
-			alpafantmeat		: 'Viande de Alpafant',
+			'.'				: 'Nourritures',
+			alpafantmeat			: 'Viande de Alpafant',
 			anurashroom			: 'Champi Anura',
 			avianpepper			: 'Poivre d\u0027Avian', // Poivre d'Avian
-			bleekerthigh		: 'Cuisse de Bleeker',
+			bleekerthigh			: 'Cuisse de Bleeker',
 			carrant				: 'Carseille',
 			commonwheat			: 'Blé Commun',
 			dunerice			: 'Riz de Dune',
@@ -88,27 +88,27 @@ module.exports = {
 			puffleegg			: 'Oeufs de Puffle',
 			ridgefennel			: 'Fenouil de Crête',
 			roseberry			: 'Baie Rose',
-			telkinchives		: 'Ciboulette de Telkin',
+			telkinchives			: 'Ciboulette de Telkin',
 			tingflower			: 'Fleur d\u0027Acmella', //Fleur d'Acmella
-			waddletoothloin		: 'Longe de Waddletooth'
+			waddletoothloin			: 'Longe de Waddletooth'
 		},
 		idea: {
-			'.'					: 'Idées',
+			'.'				: 'Idées',
 			chest				: 'Coffres',
 			pickup				: 'À ramasser',
 			quest				: 'Quêtes'
 		},
 		item: {
-			'.'					: 'Objets',
+			'.'				: 'Objets',
 			equip				: 'Équipements',
 			outfinding			: 'Horsvention',
 			quest				: 'Quêtes'
 		},
 		material: {
-			'.'					: 'Matériaux',
-			alpafantleather		: 'Cuir de Alpafant',
+			'.'				: 'Matériaux',
+			alpafantleather			: 'Cuir de Alpafant',
 			beagalite			: 'Beagalite',
-			bleekerantenna		: 'Antenne de Bleeker',
+			bleekerantenna			: 'Antenne de Bleeker',
 			crassbone			: 'Os Commun',
 			dryclay				: 'Argile Sèche',
 			dullrock			: 'Pierre de Dull',
@@ -118,7 +118,7 @@ module.exports = {
 			lunarodos			: 'Bois Nocturne',
 			marrwood			: 'Bois de Marr',
 			morrowhay			: 'Foin Doux',
-			pufflefeather		: 'Plume de Puffle',
+			pufflefeather			: 'Plume de Puffle',
 			sandstone			: 'Grès de Sable',
 			slickpearl			: 'Perle Mirroir',
 			softglass			: 'Verre Poli',
@@ -127,40 +127,40 @@ module.exports = {
 			stiffrope			: 'Stiffrope',
 			stuffcloth			: 'Corde de Bois',
 			toothstone			: 'Pierre de Dents',
-			waddletoothblubber	: 'Gelée de Waddletooth'
+			waddletoothblubber		: 'Gelée de Waddletooth'
 		},
 		mechanic: {
-			'.'					: 'Mécanismes',
+			'.'				: 'Mécanismes',
 			door				: 'Portes',
-			electrotrigger		: 'Déclencheurs électriques',
+			electrotrigger			: 'Déclencheurs électriques',
 			hittrigger			: 'Déclencheurs normaux',
 			lever				: 'Leviers',
 			pinsocket			: 'Prises de broche' // ??? check in-game name
 		},
 		nest: {
-			'.'					: 'Nid',
+			'.'				: 'Nid',
 			alpafant			: 'Alpafant',
 			bleeker				: 'Bleeker',
 			puffle				: 'Puffle',
 			waddletooth			: 'Waddletooth'
 		},
 		npc: {
-			'.'					: 'PNJs', // or « Personnages non-joueurs »
+			'.'				: 'PNJs', // or « Personnages non-joueurs »
 			chief				: 'Chefs',
 			merchant			: 'Marchants',
 			quest				: 'Quêtes',
 			village				: 'Villages'
 		},
 		spawn: {
-			'.'					: 'Points d\u0027apparition', // Points d'apparition
+			'.'				: 'Points d\u0027apparition', // Points d'apparition
 			alpafant			: 'Alpafant',
 			bleeker				: 'Bleeker',
 			puffle				: 'Puffle',
 			waddletooth			: 'Waddletooth'
 		},
 		unique: {
-			'.'					: 'Objets unique',
-			amphiscusorb		: 'Orbes',
+			'.'				: 'Objets unique',
+			amphiscusorb			: 'Orbes',
 			journal				: 'Journal / Livre',
 			keygraphite			: 'Graphite de Clé'
 		}
